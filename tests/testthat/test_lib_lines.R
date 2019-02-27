0669324c5c88b18b4e0c8ed1f8788e221568412e
Zixin Zhang <zxzzhangg@gmail.com>
2019-02-26 21:18:33 -0800

    add test for lib_lines

--- tests/testthat/test_lib_lines.R
+++ tests/testthat/test_lib_lines.R
@@ -1,13 +1,37 @@
 # input: a script
-input <- "tests/scripts/summarized_data.R"
+input <- "tests/script_for_test1.R"
 
-# expected output: a text file documenting the dependancies
-exp_output <- read_file("tests/sample.txt")
+# expected output: a list that has the information of number of lines and characters
+exp_output <- list(num_line = 7, num_char = 193)
 
-
-test_that("The Text file generated is as expected with simple case", {
-
-  expect_equal(read_file(lib_writeme("tests/scripts/summarized_data.R")), exp_output)
-
+test_that("In case the input is not a .R script", {
+  
+  expect_error(lib_lines("test.txt"))
+  expect_error(lib_lines("test.pdf"))
+  
 })
+
+test_that("If user specifies an additional argument, it throws an error", {
+  
+  expect_error(lib_lines(input, "script"))
+  
+})
+
+test_that("In case the input is not a string", {
+  
+  expect_error(lib_lines(123))
+  expect_error(lib_lines(c(1,2,3)))
+})
+
+
+test_that("test if the the ouput is correct", {
+  
+  expect_equal(lib_lines(input), exp_output)
+})
+
+
+
+
+
+
 
